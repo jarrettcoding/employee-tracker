f9a74cad5b9bfb1b09e92bd6e41aceb998b52cd1
veritascoding <jarrettcoding@gmail.com>
2022-04-29 22:32:34 -0500

    add queries

---
+++ utils/queries.js
@@ -0,0 +1,33 @@
+const db = require("../db/connection");
+
+const getAllDepartments = () => {
+  const sql = `SELECT * FROM DEPARTMENTS`;
+
+  db.query(sql, (err, rows) => {
+    console.table(rows);
+  });
+};
+
+const getAllRoles = () => {
+  const sql = `SELECT role.*, department.name
+                AS department
+                FROM role
+                LEFT JOIN department
+                ON role.department_id = department.id`;
+
+  db.query(sql, (err, rows) => {
+    console.table(rows);
+  });
+};
+
+const getAllEmployees = () => {
+  const sql = `SELECT employee.*, role.name
+                    AS role
+                    FROM employee
+                    LEFT JOIN role
+                    ON employee.role_id = role.id`;
+
+  db.query(sql, (err, rows) => {
+    console.table(rows);
+  });
+};
